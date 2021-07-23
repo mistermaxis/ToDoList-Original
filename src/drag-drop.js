af688fe6bbf410b660b04b3a052df009e9ebe7a3
mistermaxis <maximiliano_diaz@outlook.com.ar>
2021-07-23 16:15:11 -0300

    Add drag/drop functionality.

--- src/drag-drop.js
+++ src/drag-drop.js
@@ -1,5 +1,21 @@
 export default class DragDropTask {
-  static Drag(taskItem) {
-    taskItem.index += 1;
+  static updateArray(taskList, srcIdx, trgtIdx) {
+    const source = parseInt(srcIdx, 10);
+    const target = parseInt(trgtIdx, 10);
+
+    const temp = taskList[target].index;
+
+    if (source < target) {
+      for (let i = source + 1; i <= target; i += 1) {
+        taskList[i].index -= 1;
+      }
+      taskList[source].index = temp;
+    }
+    if (source > target) {
+      for (let i = target; i < source; i += 1) {
+        taskList[i].index += 1;
+      }
+      taskList[source].index = temp;
+    }
   }
 }
