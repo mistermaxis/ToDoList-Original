0633934fd9c0bb79b177d914f455cc1a8bf310dc
mistermaxis <maximiliano_diaz@outlook.com.ar>
2021-07-23 16:16:17 -0300

    Modify tasklist.js to account for drag/drop.

--- src/tasklist.js
+++ src/tasklist.js
@@ -1,119 +1,132 @@
-import Task from './task.js';
 import CompletedTask from './completed.js';
+import DragDropTask from './drag-drop.js';
+import Storage from './storage.js';
 import DragIcon from './drag-icon.svg';
 
 export default class TaskList {
     static taskList = [];
 
+    static itemID = Number;
+
+    static taskListContainer;
+
     constructor() {
-        TaskList.taskList.push(new Task('Eat', false, 1));
-        TaskList.taskList.push(new Task('Drink', false, 2));
-        TaskList.taskList.push(new Task('Sleep', false, 5));
-        TaskList.taskList.push(new Task('Walk', false, 4));
-        TaskList.taskList.push(new Task('Sit', false, 3));
+      TaskList.itemID = 0;
+      /*
+      
+      */
+      Storage.loadFromStorage(TaskList.taskList);
+      TaskList.taskListContainer = document.createElement('div');
+      TaskList.updateList();
     }
 
     static updateTaskComplete(event) {
-        const checkbox = event.currentTarget;
-        const listContainer = checkbox.parentElement;
+      const checkbox = event.currentTarget;
+      const listContainer = checkbox.parentElement;
 
-        if (checkbox.checked === true) {
-            checkbox.nextSibling.classList.add('complete');
-        } else if (checkbox.checked === false) {
-            checkbox.nextSibling.classList.remove('complete');
-        }
-        CompletedTask.updateCompleted(TaskList.taskList, listContainer.id);
+      if (checkbox.checked === true) {
+        checkbox.nextSibling.classList.add('complete');
+      } else if (checkbox.checked === false) {
+        checkbox.nextSibling.classList.remove('complete');
+      }
+      CompletedTask.updateCompleted(TaskList.taskList, listContainer.id);
     }
 
     static onDragStart(event) {
-        setTimeout(() => {
-            event.target.classList.remove('visible');
-            event.target.classList.add('hidden');
-        }, 0);
+      const item = event.currentTarget;
+      item.classList.add('dragged');
+      event.dataTransfer.setData('text/plain', item.id);
+      setTimeout(() => {
+        item.classList.remove('visible');
+        item.classList.add('hidden');
+      }, 0);
+    }
+
+    static onDragOver(event) {
+      event.preventDefault();
     }
 
     static onDragEnd(event) {
-        console.log(event.target);
-        event.target.classList.remove('hidden');
-        event.target.classList.add('visible');
+      const item = event.currentTarget;
+      item.classList.remove('hidden');
+      item.classList.add('dragged');
+      item.classList.add('visible');
     }
 
-    static onDragOver() {
+    static onDrop(event) {
+      const sourceIndex = event.dataTransfer.getData('text');
+      const targetIndex = event.currentTarget.id;
 
-    }
+      DragDropTask.updateArray(TaskList.taskList, sourceIndex, targetIndex);
 
-    static onDrop() {
+      TaskList.updateList();
 
+      Storage.saveToStorage(TaskList.taskList);
     }
 
     static changeToDrag(event) {
-        console.log('Mouse Over Icon');
-        const itemToDrag = event.currentTarget.parentElement;
+      const itemToDrag = event.currentTarget.parentElement;
 
-        itemToDrag.draggable = true;
+      itemToDrag.draggable = true;
 
-        console.log(`Draggable = ${itemToDrag.draggable}`);
-
-        itemToDrag.addEventListener('dragstart', TaskList.onDragStart);
-        itemToDrag.addEventListener('dragover', TaskList.onDragOver);
-        itemToDrag.addEventListener('dragend', TaskList.onDragEnd);
-        itemToDrag.addEventListener('drop', TaskList.onDrop);
+      itemToDrag.addEventListener('dragstart', TaskList.onDragStart);
+      itemToDrag.addEventListener('dragend', TaskList.onDragEnd);
     }
 
     static changeToNoDrag(event) {
-        console.log('Mouse Over Icon');
+      const itemNoDrag = event.currentTarget.parentElement;
 
-        const itemNoDrag = event.currentTarget.parentElement;
+      itemNoDrag.draggable = false;
 
-        itemNoDrag.draggable = false;
-
-        console.log(`Draggable = ${itemNoDrag.draggable}`);
-
-        itemNoDrag.removeEventListener('dragstart', TaskList.onDragStart);
-        itemNoDrag.removeEventListener('dragover', TaskList.onDragOver);
-        itemNoDrag.removeEventListener('dragend', TaskList.onDragEnd);
-        itemNoDrag.removeEventListener('drop', TaskList.onDrop);
+      itemNoDrag.removeEventListener('dragstart', TaskList.onDragStart);
+      itemNoDrag.removeEventListener('dragend', TaskList.onDragEnd);
     }
 
-    taskListComponent() {
-        TaskList.taskList.sort((a, b) => a.index - b.index);
+    get taskListComponent() {
+      return TaskList.taskListContainer;
+    }
 
-        const taskListContainer = document.createElement('div');
+    static updateList() {
+      TaskList.taskList.sort((a, b) => a.index - b.index);
 
-        TaskList.taskList.forEach((task) => {
-            const listItem = document.createElement('div');
-            listItem.classList.add('list-item');
-            listItem.classList.add('flex-list-item');
-            listItem.id = task.index;
+      TaskList.taskListContainer.innerHTML = '';
 
-            const itemCompleted = document.createElement('input');
-            itemCompleted.type = 'checkbox';
-            itemCompleted.checked = task.completed;
-            itemCompleted.classList.add('item-completed');
-            itemCompleted.addEventListener('change', TaskList.updateTaskComplete);
+      TaskList.taskList.forEach((task) => {
+        const listItem = document.createElement('div');
+        listItem.classList.add('list-item');
+        listItem.classList.add('flex-list-item');
+        listItem.id = task.index;
 
-            const itemDescription = document.createElement('label');
-            itemDescription.innerText = task.description;
-            itemDescription.classList.add('item-description');
+        const itemCompleted = document.createElement('input');
+        itemCompleted.type = 'checkbox';
+        itemCompleted.checked = task.completed;
+        itemCompleted.classList.add('item-completed');
+        itemCompleted.addEventListener('change', TaskList.updateTaskComplete);
 
-            const dragButton = document.createElement('button');
+        const itemDescription = document.createElement('label');
+        itemDescription.innerText = task.description;
+        itemDescription.classList.add('item-description');
 
-            const itemIcon = document.createElement('img');
-            itemIcon.classList.add('drag-icon');
-            itemIcon.src = DragIcon;
+        const dragButton = document.createElement('button');
 
-            dragButton.appendChild(itemIcon);
-            dragButton.classList.add('drag-button');
-            dragButton.classList.add('flex-list-item');
+        const itemIcon = document.createElement('img');
+        itemIcon.classList.add('drag-icon');
+        itemIcon.src = DragIcon;
 
-            dragButton.addEventListener('mouseover', TaskList.changeToDrag);
-            dragButton.addEventListener('mouseleave', TaskList.changeToNoDrag);
+        dragButton.appendChild(itemIcon);
+        dragButton.classList.add('drag-button');
+        dragButton.classList.add('flex-list-item');
 
-            listItem.appendChild(itemCompleted);
-            listItem.appendChild(itemDescription);
-            listItem.appendChild(dragButton);
-            taskListContainer.appendChild(listItem);
-        });
-        return taskListContainer;
+        dragButton.addEventListener('mouseover', TaskList.changeToDrag);
+        dragButton.addEventListener('mouseleave', TaskList.changeToNoDrag);
+
+        listItem.addEventListener('drop', TaskList.onDrop);
+        listItem.addEventListener('dragover', TaskList.onDragOver);
+
+        listItem.appendChild(itemCompleted);
+        listItem.appendChild(itemDescription);
+        listItem.appendChild(dragButton);
+        TaskList.taskListContainer.appendChild(listItem);
+      });
     }
 }
